ab7dcb25cae9c36252103cec3ba9badf0e5b7e22
Mariajosehid <mjhidalgo@mail.austral.edu.ar>
2025-09-29 10:58:14 -0300

    Add files via upload

--- lists.py
+++ lists.py
@@ -1,20 +1,32 @@
-# Replace the "ANSWER HERE" with your answer
-
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def remove_elements(lst):
+    new_list = lst[:]
+    for i in sorted([0, 4, 5], reverse=True):
+        if i < len(new_list):
+            new_list.pop(i)
+    return new_list
 
 
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+def add_elements(lst):
+    new_list = ['Pink'] + lst + ['Yellow']
+    return new_list
 
 
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+def is_empty(lst):
+    return len(lst) == 0
 
 
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def check_lists(lst1, lst2):
+    if len(lst1) >= 3 and len(lst2) >= 3:
+        return lst1[2] == lst2[2]
+    return False
+
+
+def list_of_lists(big_list):
+    new_list = []
+    if len(big_list) >= 1:
+        new_list.append(big_list[0][:2])
+    if len(big_list) >= 2:
+        new_list.append(big_list[1][1:4])
+    if len(big_list) >= 3:
+        new_list.append(big_list[2][-2:])
+    return new_list
